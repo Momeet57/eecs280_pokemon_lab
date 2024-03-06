5657cefde57644a8fe48da20f8dab8eeaa756ef1
Matan Marcus-Neuman <matanmn@umich.edu>
2024-03-05 21:10:54 -0500

    Co-authored-by: prestonis@outlook.com <prestonis@outlook.com> Co-authored-by: mariaparsa <mariaparsa@users.noreply.github.com> Co-authored-by: aligallaghan <aligallaghan@users.noreply.github.com>

--- Trainer.cpp
+++ Trainer.cpp
@@ -1,24 +1,22 @@
 
-#include <algorithm>
-#include <random>
-#include <string>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <random>
-
 #include "Trainer.hpp"
 
+#include <algorithm>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
 Trainer::Trainer(const std::string &name)
-  : name(name),
-    pokemon({
-      {"Pikachu", 1, ELECTRIC},
-      {"Charmander", 1, FIRE},
-      {"Squirtle", 1, WATER},
-      {"Bulbasaur", 1, GRASS},
-      {"Cyndaquil", 1, FIRE},
-    }),
-    active_roster (pokemon) {
+    : name(name),
+      pokemon({
+          {"Pikachu", 1, ELECTRIC},
+          {"Charmander", 1, FIRE},
+          {"Squirtle", 1, WATER},
+          {"Bulbasaur", 1, GRASS},
+          {"Cyndaquil", 1, FIRE},
+      }),
+      active_roster(pokemon) {
   // nothing to do here
 }
 
@@ -26,7 +24,7 @@ Trainer::Trainer(std::istream &is) {
   is >> name;
 
   // Assumption: Read exactly 5 pokemon
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     Pokemon p;
     is >> p;
     pokemon.push_back(p);
@@ -37,15 +35,15 @@ Trainer::Trainer(std::istream &is) {
 }
 
 // EFFECTS: Returns the name of the trainer
-const std::string & Trainer::get_name() const {
-  return name;
-}
+const std::string &Trainer::get_name() const { return name; }
 
 // EFFECTS: Arbitrarily chooses a pokemon for battle by selecting the first
 //          pokemon in the trainer's active roster. That pokemon is removed
 //          from the roster and returned.
 Pokemon Trainer::choose_pokemon() {
-  return Pokemon(); // TODO: Replace with your implementation
+  Pokemon chosen = this->active_roster[0];
+  (this->active_roster).erase((this->active_roster).begin());
+  return chosen;
 }
 
 // EFFECTS: Chooses a pokemon to face a specific adversary type. Specifically,
@@ -54,31 +52,33 @@ Pokemon Trainer::choose_pokemon() {
 //          pokemon, the first pokemon in the roster is returned. The chosen
 //          pokemon is removed from the roster.
 Pokemon Trainer::choose_pokemon(PokemonType adversary_type) {
-  return Pokemon(); // TODO: Replace with your implementation
+  Pokemon chosen = this->active_roster[0];
+  for (int i = 0; i < this->active_roster.size(); i++) {
+    if ((this->active_roster[i]).is_effective_against(adversary_type)) {
+      chosen = this->active_roster[i];
+      (this->active_roster).erase((this->active_roster).begin() + i);
+      return chosen;
+    }
+  }
+  return choose_pokemon();
 }
 
 // EFFECTS: Resets the trainers active roster so that it contains all
 //          of their pokemon.
-void Trainer::reset() {
-  active_roster = pokemon;
-}
+void Trainer::reset() { active_roster = pokemon; }
 
 // EFFECTS: Creates a trainer with the given name and the
 //          default set of pokemon.
-Trainer * Trainer_factory(const std::string &name) {
-  return new Trainer(name);
-}
+Trainer *Trainer_factory(const std::string &name) { return new Trainer(name); }
 
 // REQUIRES: The stream input contains valid trainer information
 // EFFECTS: Creates a dynamically allocated Trainer and returns
 //          a pointer to it. Don't forget to use delete to free the
 //          Trainer when you're finished with it!
-Trainer * Trainer_factory(std::istream &is) {
-  return new Trainer(is);
-}
+Trainer *Trainer_factory(std::istream &is) { return new Trainer(is); }
 
 // EFFECTS: Prints the name of the trainer to the given stream
-std::ostream & operator<<(std::ostream &os, const Trainer &trainer) {
+std::ostream &operator<<(std::ostream &os, const Trainer &trainer) {
   os << trainer.get_name();
   return os;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <string>
 
 #include "Pokemon.hpp"
 #include "Trainer.hpp"
@@ -10,37 +9,86 @@
 using namespace std;
 
 class League {
-private:
-  // TODO: Add member variables here
+ private:
+  vector<Trainer *> gym_leaders;
+  int num_gym_leaders;
+  Trainer *player;
+  vector<Trainer *> defeated;
 
-public:
+ public:
+  League(int num_gym_leads, istream &is) {
+    num_gym_leaders = num_gym_leads;
+    string gym;
+    is >> gym;
+    for (int i = 0; i < num_gym_leaders; i++) {
+      gym_leaders[i] = Trainer_factory(is);
+    }
+  }
 
-  // TODO: Add constructor here to initialize members
-  
+  void print_pokemon_battles(Trainer *current_gym_leader) {
+    int num_wins = 0;
+    for (int i = 0; i < 5; ++i) {
+      // Each individual pokemon battle
+      Pokemon enemy = current_gym_leader->choose_pokemon();
+      cout << *current_gym_leader << " chooses " << enemy << endl;
+      Pokemon p = player->choose_pokemon(enemy.get_type());
+      cout << *player << " chooses " << p << endl;
+      if (Pokemon_battle(p, enemy)) {
+        cout << p << " defeats " << enemy << endl << endl;
+        ++num_wins;
+      } else {
+        cout << enemy << " defeats " << p << endl << endl;
+      }
+    }
+    cout << "Result: " << *player << "=" << num_wins << ", "
+         << *current_gym_leader << "=" << 5 - num_wins << endl;
+  }
 
-
-
-  // TODO: Add member functions here
-
-
-
-  
-
+  void gym_battles() {
+    for (int i = 0; i < gym_leaders.size(); ++i) {
+      player->reset();
+      cout << "-----" << *player << " vs. " << *gym_leaders[i] << "-----"
+           << endl;
+      print_pokemon_battles(gym_leaders[i]);
+    }
+  }
+  void print_final_results() {
+    cout << *player << " won " << defeated.size()
+         << " matches by defeating:" << endl;
+    for (int i = 0; i < defeated.size(); ++i) {
+      cout << *defeated[i] << endl;
+    }
+  }
   ~League() {
     // TODO: put code here to clean up by deleting all Trainer objects
-    
   }
-  
 };
 
 int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    cout << "Usage: battle.exe PLAYER_FILE GYM_LEADERS_FILE" << endl;
+    return 1;
+  }
 
-  // TODO: Add code to read command line args and open file streams here
-  
+  std::string player_in_name = argv[1];
+  std::string gym_leaders_in_name = argv[2];
 
+  std::ifstream player_in(player_in_name);
+  if (!player_in.is_open()) {
+    cout << "Unable to open " << player_in_name << endl;
+    return 1;
+  }
+  std::ifstream gym_in(gym_leaders_in_name);
+  if (!gym_in.is_open()) {
+    cout << "Unable to open " << gym_leaders_in_name << endl;
+    return 1;
+  }
+  int numGymLeads;
+  gym_in >> numGymLeads;
+  League league(numGymLeads, gym_in);
+  Trainer_factory(player_in);
+  league.gym_battles();
+  league.print_final_results();
 
-  // TODO: Create a League object, call function to run the simulation
-  
-
-  
+  return 0;
 }
